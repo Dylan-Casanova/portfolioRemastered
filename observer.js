f7919b3aec7bb3457ec690e58603aba45490ef49
Dylan Casanova <dyylancasanova@hotmail.com>
2021-11-04 15:25:12 -0500

    making footer stay at all times to display email information

--- observer.js
+++ observer.js
@@ -1,16 +1,23 @@
 const navbar = document.querySelector("nav");
 const section1= document.querySelector("#background");
 
+const footer = document.querySelector("footer");
+
+
 section1options = {
     rootMargin: "-200px 0px 0px 0px"
 };
 
+
 const observer = new IntersectionObserver(function(entries,observer ) {
     entries.forEach(entry => {
         if (!entry.isIntersecting){
-            navbar.classList.add("nav1");
+            navbar.classList.add("nav1")
+            footer.classList.add("fixed-bottom")
+            ;
         } else {
             navbar.classList.remove("nav1")
+            footer.classList.remove("fixed-bottom")
         }
     });
 }, section1options);
